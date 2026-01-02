84c0f5389f715154a6c6e1599d86abb232b5d905
Valentin <valentin.haub@mni.thm.de>
2026-01-03 00:12:57 +0100

    Implement FreeRTOS task architecture and queue messaging

--- src/main.cpp
+++ src/main.cpp
@@ -2,37 +2,65 @@
 #include "TrafficLight.hpp"
 
 
-#define PIN_TASTER_REQUEST 13
+#define PIN_REQUEST 13
 
 TrafficLight tl;
 
+QueueHandle_t q;
+TaskHandle_t hTrafficLight;
+TaskHandle_t hRequest;
+
+
+void taskTrafficLight(void* pvParemeters){
+  TrafficLight_EventId event;
+
+  TrafficLight_ctor(&tl);
+  TrafficLight_start(&tl);
+  
+  while (1){
+    if (xQueueReceive(q, &event, portMAX_DELAY) == pdPASS){
+      TrafficLight_dispatch_event(&tl, event);
+    }
+  }
+}
+
+void taskRequest(void* pvParameters){
+  int lastRequestState = HIGH;
+  int currentRequestState;
+
+  pinMode(PIN_REQUEST, INPUT_PULLUP);
+
+  while(1){
+    currentRequestState = digitalRead(PIN_REQUEST);
+
+    if (lastRequestState == HIGH && currentRequestState == LOW) {
+      TrafficLight_EventId request = TrafficLight_EventId_REQUESTGREEN;
+      xQueueSend(q, &request, 0);
+    }
+    lastRequestState = currentRequestState;
+
+    vTaskDelay(pdMS_TO_TICKS(20));
+  }
+}
+
 
 unsigned long lastTickTime = 0;
-int lastRequestState = HIGH;
-
 
 void setup() {
-    Serial.begin(115200);
-    
-    pinMode(PIN_TASTER_REQUEST, INPUT_PULLUP);
+  Serial.begin(115200);
 
-    TrafficLight_ctor(&tl); 
-    TrafficLight_start(&tl);
+  q = xQueueCreate(20, sizeof(TrafficLight_EventId));
+
+  xTaskCreate(taskTrafficLight, "TrafficLight Task", 2048, NULL, 2, &hTrafficLight);
+  xTaskCreate(taskRequest, "Request Task", 1024, NULL, 1, &hRequest);
+     
 }
 
 
 void loop() {
-    
-    // Taster "requestGreen" prüfen
-    int currentRequestState = digitalRead(PIN_TASTER_REQUEST);
-    if (lastRequestState == HIGH && currentRequestState == LOW) {
-        TrafficLight_dispatch_event(&tl, TrafficLight_EventId_REQUESTGREEN);
-    }
-    lastRequestState = currentRequestState;
-
-    // Zeit-Event "TICK" senden (1 Tick pro Millisekunde)
-    if (millis() - lastTickTime >= 1) {
-        lastTickTime = millis();
-        TrafficLight_dispatch_event(&tl, TrafficLight_EventId_TICK); 
-    }
+  // Zeit-Event "TICK" senden (1 Tick pro Millisekunde)
+  if (millis() - lastTickTime >= 1) {
+    lastTickTime = millis();
+    TrafficLight_dispatch_event(&tl, TrafficLight_EventId_TICK);
+  }
 }
