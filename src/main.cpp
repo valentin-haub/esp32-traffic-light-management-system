2085a8718fe84dc56a7c6da59839f3fbfcad08ca
Valentin <valentin.haub@mni.thm.de>
2026-01-03 00:37:49 +0100

    Add SerialTask for UART interaction (get current state, set request)

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ QueueHandle_t q;
 TaskHandle_t hTrafficLight;
 TaskHandle_t hRequest;
 TaskHandle_t hClock;
+TaskHandle_t hSerial;
 
 
 void taskTrafficLight(void* pvParemeters){
@@ -45,13 +46,57 @@ void taskRequest(void* pvParameters){
 }
 
 void taskClock(void* pvParameters){
-    TrafficLight_EventId tick = TrafficLight_EventId_TICK;
+  TrafficLight_EventId tick = TrafficLight_EventId_TICK;
 
-    while(1){
-        xQueueSend(q, &tick, 0);
+  while(1){
+    xQueueSend(q, &tick, 0);
 
-        vTaskDelay(pdMS_TO_TICKS(1));
+    vTaskDelay(pdMS_TO_TICKS(1));
+  }
+}
+
+void taskSerial(void* pvParameters){
+  char input;
+  const char* menu = "Wählen Sie einen Eintrag per Eingabe der Nummer:\n"
+                      "1. Auslesen des Leuchzustands einer Ampel\n"
+                      "2. Setzen des Requests bei einer Ampel\n\n"
+                      "Auswahl: ";
+
+  Serial.print(menu);
+
+  while(1){
+    if (Serial.available() > 0){
+      input = Serial.read();
+
+      if (input == '1' || input == '2'){
+        Serial.print(input);
+        Serial.println();
+
+        if (input == '1'){
+          const char* currentState = TrafficLight_state_id_to_string(tl.state_id);
+          Serial.print("Der aktuelle Zustand der Ampel ist: ");
+          Serial.print(currentState);
+        }
+        else if (input == '2'){
+          TrafficLight_EventId request = TrafficLight_EventId_REQUESTGREEN;
+          xQueueSend(q, &request, 0);
+          Serial.print("Request wurde gesetzt.");
+        }
+
+        Serial.println();
+        Serial.println();
+        Serial.print(menu);
+      }
+      else {
+        Serial.println();
+        Serial.println("\nUngueltige Eingabe. Bitte '1' oder '2' eingeben.\n\n");
+        Serial.print(menu);
+      }
     }
+    else {
+      vTaskDelay(pdMS_TO_TICKS(20));
+    }
+  }
 }
 
 
@@ -64,6 +109,7 @@ void setup() {
   xTaskCreate(taskClock, "Clock Task", 1024, NULL, 3, &hClock);
   xTaskCreate(taskTrafficLight, "TrafficLight Task", 2048, NULL, 2, &hTrafficLight);
   xTaskCreate(taskRequest, "Request Task", 1024, NULL, 1, &hRequest);
+  xTaskCreate(taskSerial, "Serial Task", 4096, NULL, 1, &hSerial);
 }
 
 
