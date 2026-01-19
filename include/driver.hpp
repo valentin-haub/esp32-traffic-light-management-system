0bdf57f75456d4765cf0c58f462426967b47fdbd
Valentin <valentin.haub@mni.thm.de>
2026-01-19 19:45:17 +0100

    Hilfsmethoden in driver geschrieben: Anzeigen einer 4-Bit-Zahl an LEDs

--- include/driver.hpp
+++ include/driver.hpp
@@ -7,3 +7,7 @@ void initLight(uint8_t pin);
 void turnOnLight(uint8_t pin);
 
 void turnOffLight(uint8_t pin);
+
+
+void initBinaryDisplay(uint8_t p0, uint8_t p1, uint8_t p2, uint8_t p3);
+void refreshBinaryDisplay(uint8_t value, uint8_t p0, uint8_t p1, uint8_t p2, uint8_t p3);
--- src/driver.cpp
+++ src/driver.cpp
@@ -13,3 +13,25 @@ void turnOnLight(uint8_t pin) {
 void turnOffLight(uint8_t pin) {
     digitalWrite(pin, LOW);
 }
+
+
+void initBinaryDisplay(uint8_t p0, uint8_t p1, uint8_t p2, uint8_t p3){
+    pinMode(p0, OUTPUT);
+    digitalWrite(p0, LOW);
+
+    pinMode(p1, OUTPUT);
+    digitalWrite(p1, LOW);
+
+    pinMode(p2, OUTPUT);
+    digitalWrite(p2, LOW);
+
+    pinMode(p3, OUTPUT);
+    digitalWrite(p3, LOW);
+}
+
+  void refreshBinaryDisplay(uint8_t value, uint8_t p0, uint8_t p1, uint8_t p2, uint8_t p3){
+    digitalWrite(p0, (value & 0x01) ? HIGH : LOW);
+    digitalWrite(p1, (value & 0x02) ? HIGH : LOW);
+    digitalWrite(p2, (value & 0x04) ? HIGH : LOW);
+    digitalWrite(p3, (value & 0x08) ? HIGH : LOW);
+  }
