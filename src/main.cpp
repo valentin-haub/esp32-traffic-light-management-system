cd12909270f8ab50d7eb88085d916aa7ca33abf6
Valentin <valentin.haub@mni.thm.de>
2026-01-19 17:53:40 +0100

    Zweite Ampel in eigenen Task aufgelagert

--- src/main.cpp
+++ src/main.cpp
@@ -22,27 +22,23 @@ TrafficLight tl1;
 TrafficLight tl2;
 
 QueueHandle_t q;
-TaskHandle_t hTrafficLight;
+TaskHandle_t hTrafficLight1;
+TaskHandle_t hTrafficLight2;
 TaskHandle_t hRequest;
 TaskHandle_t hClock;
 TaskHandle_t hSerial;
 TaskHandle_t hSensors;
 
 
-void taskTrafficLight(void* pvParemeters){
+void taskTrafficLight1(void* pvParemeters){
   TrafficLight_EventId event;
 
   TrafficLight_ctor(&tl1);
-  TrafficLight_ctor(&tl2);
 
   // Pins setzen
   tl1.vars.pinRed = PIN_RED_1;
   tl1.vars.pinYellow = PIN_YELLOW_1;
   tl1.vars.pinGreen = PIN_GREEN_1;
-
-  tl2.vars.pinRed = PIN_RED_2;
-  tl2.vars.pinYellow = PIN_YELLOW_2;
-  tl2.vars.pinGreen = PIN_GREEN_2;
 
   // Startwerte setzen
   tl1.vars.time = 0;
@@ -50,12 +46,31 @@ void taskTrafficLight(void* pvParemeters){
   tl1.vars.brightness = 4095; // Start als "hell"
   tl1.vars.threshold = 2000;
 
+  TrafficLight_start(&tl1);
+  
+  while (1){
+    if (xQueueReceive(q, &event, portMAX_DELAY) == pdPASS){
+      TrafficLight_dispatch_event(&tl1, event);
+    }
+  }
+}
+
+void taskTrafficLight2(void* pvParemeters){
+  TrafficLight_EventId event;
+
+  TrafficLight_ctor(&tl2);
+
+  // Pins setzen
+  tl2.vars.pinRed = PIN_RED_2;
+  tl2.vars.pinYellow = PIN_YELLOW_2;
+  tl2.vars.pinGreen = PIN_GREEN_2;
+
+  // Startwerte setzen
   tl2.vars.time = 0;
   tl2.vars.greenTime = 5000; // 5 Sekunden
   tl2.vars.brightness = 4095; // Start als "hell"
   tl2.vars.threshold = 2000;
 
-  TrafficLight_start(&tl1);
   TrafficLight_start(&tl2);
   
   while (1){
