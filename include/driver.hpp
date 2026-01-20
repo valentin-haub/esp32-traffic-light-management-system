e302603e187e3c5b30fd24edf8c178301275b05a
Valentin <valentin.haub@mni.thm.de>
2026-01-20 10:42:35 +0100

    driver.hpp um Hilfsfunktionen für das Servo-System ergänzt

--- include/driver.hpp
+++ include/driver.hpp
@@ -10,4 +10,15 @@ void turnOffLight(uint8_t pin);
 
 
 void initBinaryDisplay(uint8_t p0, uint8_t p1, uint8_t p2, uint8_t p3);
+
+// Aktualisiert die Status-LEDs
 void refreshBinaryDisplay(uint8_t value, uint8_t p0, uint8_t p1, uint8_t p2, uint8_t p3);
+
+
+void initBarrierSystem(uint8_t servoPin, uint8_t sdaPin, uint8_t sclPin);
+
+// Gibt die Neigung (Y-Achse) zurück
+float getBarrierInclination(); 
+
+// Setzt den Servo auf einen Winkel (0-180)
+void setBarrierServo(int angle);
