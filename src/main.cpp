99f4d5d78e1871f18b87001d519f07ed49101328
Valentin <valentin.haub@mni.thm.de>
2026-01-20 17:41:31 +0100

    Mapping des Winkels korrigiert

--- src/main.cpp
+++ src/main.cpp
@@ -275,7 +275,7 @@ void taskSensors(void* pvParameters){
     // Auslesen der Neigung des Hebels und Berechnen der Neigung
     float inclination = getBarrierInclination(); // Neigung Y-Achse
 
-    int targetAngle = map((int)(inclination * 10), 0, 98, 0, 180); // 0 - 9.8 --> 0 - 90
+    int targetAngle = map((int)(inclination * 10), 0, 98, 0, 90); // 0 - 9.8 --> 0 - 90
     targetAngle = constrain(targetAngle, 0, 90); // Begrenzung
 
     // Anzeigen auf dem Display
